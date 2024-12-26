39522626d3084d2097341f1496cce30d045d82fc
Xabier Pérez Del Val <xabierperezdelval@gmail.com>
2024-12-26 02:20:22 +0100

    added Controller library

---
+++ scripts/controller-handler.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  Controller.search();
+});
+
+window.addEventListener(
+  "gc.controller.found",
+  function (event) {
+    var controller = event.detail.controller;
+    console.log("Controller found at index " + controller.index + ".");
+    console.log("'" + controller.name + "' is ready!");
+  },
+  false
+);
